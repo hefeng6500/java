2e742bbb91be249e3d9fdeed2209c7d2d4bb1f6e
hefeng6500 <2443992009@qq.com>
2023-04-16 17:23:47 +0800

    druid 数据库连接池测试

--- jdbc/src/com/yang/test/BrandTest.java
+++ jdbc/src/com/yang/test/BrandTest.java
@@ -116,4 +116,69 @@ public class BrandTest {
     pstmt.close();
     conn.close();
   }
+
+  /**
+   * 修改
+   * 1. SQL：
+   * <p>
+   * update tb_brand
+   * set brand_name  = ?,
+   * company_name= ?,
+   * ordered     = ?,
+   * description = ?,
+   * status      = ?
+   * where id = ?
+   * <p>
+   * 2. 参数：需要，所有数据
+   * 3. 结果：boolean
+   */
+
+  @Test
+  public void testUpdate() throws Exception {
+    // 接收页面提交的参数
+    String brandName = "香飘飘";
+    String companyName = "香飘飘";
+    int ordered = 1000;
+    String description = "绕地球三圈";
+    int status = 1;
+    int id = 4;
+
+    // 1. 获取Connection
+    // 3. 加载配置文件
+    Properties prop = new Properties();
+    prop.load(new FileInputStream("src/druid.properties"));
+    // 4. 获取连接池对象
+    DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+    // 5. 获取数据库连接 Connection
+    Connection conn = dataSource.getConnection();
+    // 2. 定义SQL
+    String sql = " update tb_brand\n" +
+            "         set brand_name  = ?,\n" +
+            "         company_name= ?,\n" +
+            "         ordered     = ?,\n" +
+            "         description = ?,\n" +
+            "         status      = ?\n" +
+            "     where id = ?";
+
+    // 3. 获取pstmt对象
+    PreparedStatement pstmt = conn.prepareStatement(sql);
+
+    // 4. 设置参数
+    pstmt.setString(1, brandName);
+    pstmt.setString(2, companyName);
+    pstmt.setInt(3, ordered);
+    pstmt.setString(4, description);
+    pstmt.setInt(5, status);
+    pstmt.setInt(6, id);
+
+    // 5. 执行SQL
+    int count = pstmt.executeUpdate(); // 影响的行数
+    // 6. 处理结果
+    System.out.println(count > 0);
+
+    // 7. 释放资源
+    pstmt.close();
+    conn.close();
+  }
+
 }
