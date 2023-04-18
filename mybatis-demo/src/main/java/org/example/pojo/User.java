67e688f6d891d7311b87c9ca7415d01aef4278bf
hefeng6500 <2443992009@qq.com>
2023-04-18 18:06:50 +0800

    feat: mybatis 学习

---
+++ mybatis-demo/src/main/java/org/example/pojo/User.java
@@ -0,0 +1,49 @@
+package org.example.pojo;
+
+public class User {
+  private int id;
+  private String username;
+  private String password;
+  private String gender;
+  private String addr;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public String getAddr() {
+    return addr;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public void setAddr(String addr) {
+    this.addr = addr;
+  }
+}
