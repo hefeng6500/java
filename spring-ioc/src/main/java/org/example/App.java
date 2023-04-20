9fc5ebebfbaccfe98e1259602eedfd0e9b978bb4
hefeng6500 <2443992009@qq.com>
2023-04-20 22:06:06 +0800

    feat: spring ioc 学习

---
+++ spring-ioc/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.dao.BookDao;
+import org.example.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+  public static void main(String[] args) {
+    // 获取IOC容器
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+    bookDao.save();
+
+    BookService bookService = (BookService) ctx.getBean("bookService");
+    bookService.save();
+  }
+}
