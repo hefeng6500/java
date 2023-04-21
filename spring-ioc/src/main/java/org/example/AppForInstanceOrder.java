4f7ab6e2b90e9d3a99d4a869c91a5eb70b735504
hefeng6500 <2443992009@qq.com>
2023-04-21 21:01:56 +0800

    feat: 学习通过示例方法创建对象和 ioc 通过实例工厂创建 bean 对象

--- spring-ioc/src/main/java/org/example/AppForInstanceOrder.java
+++ spring-ioc/src/main/java/org/example/AppForInstanceOrder.java
@@ -2,10 +2,17 @@ package org.example;
 
 import org.example.dao.OrderDao;
 import org.example.factory.OrderDaoFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppForInstanceOrder {
   public static void main(String[] args) {
-    OrderDao orderDao = OrderDaoFactory.getOrderDao();
+    // OrderDao orderDao = OrderDaoFactory.getOrderDao();
+
+    // ioc 容器通过静态工厂创建 bean
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    OrderDao orderDao = (OrderDao) ctx.getBean("orderDao");
 
     orderDao.save();
   }
---
+++ spring-ioc/src/main/java/org/example/AppForInstanceUser.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.dao.UserDao;
+import org.example.factory.UserDaoFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppForInstanceUser {
+  public static void main(String[] args) {
+
+    // 实例工厂函数创建对象
+    // UserDaoFactory userDaoFactory = new UserDaoFactory();
+    // UserDao userDao = userDaoFactory.getUserDao();
+    //
+    // userDao.save();
+
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    UserDao userDao = (UserDao) ctx.getBean("userDao");
+
+    userDao.save();
+  }
+}
