846d703cfa5059218e8a8d8ee03621dc88045ecd
hefeng6500 <2443992009@qq.com>
2023-04-22 20:02:46 +0800

    feat: ioc 容器加载 properties 文件

--- spring-third-bean/src/main/java/com/example/springthirdbean/App.java
+++ spring-third-bean/src/main/java/com/example/springthirdbean/App.java
@@ -1,5 +1,6 @@
 package com.example.springthirdbean;
 
+import com.example.dao.BookDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,7 +10,10 @@ public class App {
   public static void main(String[] args) {
     ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-    DataSource dataSource = (DataSource) ctx.getBean("dataSource");
-    System.out.println(dataSource);
+    // DataSource dataSource = (DataSourcetaSource) ctx.getBean("dataSource");
+    // System.out.println(dataSource);
+
+    BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+    bookDao.save();
   }
 }
