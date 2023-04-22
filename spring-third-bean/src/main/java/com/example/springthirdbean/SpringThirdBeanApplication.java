7bbc6ed9db75b0ed77e9e2a411d95dbd0d031f9f
hefeng6500 <2443992009@qq.com>
2023-04-22 19:40:43 +0800

    feat: IOC/DI 配置管理第三方 bean

---
+++ spring-third-bean/src/main/java/com/example/springthirdbean/SpringThirdBeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.springthirdbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThirdBeanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringThirdBeanApplication.class, args);
+  }
+
+}
---
+++ spring-third-bean/src/test/java/com/example/springthirdbean/SpringThirdBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springthirdbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThirdBeanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
