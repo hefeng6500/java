ddf3413b521656ed1bd16d5d5fb4fe7cca047482
hefeng6500 <2443992009@qq.com>
2023-04-25 21:36:24 +0800

    feat: 使用纯注解开发模式，使用 @Import引入第三方 bean 配置

--- spring-pure-annotation/src/main/java/com/example/config/SpringConfig.java
+++ spring-pure-annotation/src/main/java/com/example/config/SpringConfig.java
@@ -1,16 +1,14 @@
 package com.example.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan("com.example.config")
+// @ComponentScan("com.example.config")
 @PropertySource("jdbc.properties")
+@Import({JdbcConfig.class})
 public class SpringConfig {
 
 
