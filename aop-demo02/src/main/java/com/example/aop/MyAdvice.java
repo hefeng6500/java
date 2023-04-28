33549a314373d7881287b28b03ec6663efd73e45
hefeng6500 <2443992009@qq.com>
2023-04-28 11:35:28 +0800

    feat: aop demo02

---
+++ aop-demo02/src/main/java/com/example/aop/MyAdvice.java
@@ -0,0 +1,39 @@
+package com.example.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyAdvice {
+
+  @Pointcut("execution(* com.example.dao.BookDao.findName(..))")
+  private void pt(){}
+
+  @Before("pt()")
+  public void before() {
+    System.out.println("before advice ..." );
+  }
+
+  @After("pt()")
+  public void after() {
+    System.out.println("after advice ...");
+  }
+
+  @Around("pt()")
+  public Object around(ProceedingJoinPoint pjp) throws Throwable{
+    Object ret = pjp.proceed();
+    return ret;
+  }
+  @AfterReturning("pt()")
+  public void afterReturning() {
+    System.out.println("afterReturning advice ...");
+  }
+
+
+  @AfterThrowing("pt()")
+  public void afterThrowing() {
+    System.out.println("afterThrowing advice ...");
+  }
+}
---
+++ aop-demo02/src/main/java/com/example/dao/impl/BookDaoImpl.java
@@ -0,0 +1,13 @@
+package com.example.dao.impl;
+
+import com.example.dao.BookDao;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class BookDaoImpl implements BookDao {
+  public String findName(int id) {
+    System.out.println("id:"+id);
+    return "Hello";
+  }
+}
