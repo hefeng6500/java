1e97e8903a182560eb852a8c35eca955d119d6fb
hefeng6500 <2443992009@qq.com>
2023-05-03 18:52:50 +0800

    feat: mvc restful api 列表返回

--- springmvc-demo03/src/main/java/com/example/controller/BookController.java
+++ springmvc-demo03/src/main/java/com/example/controller/BookController.java
@@ -4,6 +4,9 @@ import com.example.domain.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController // @Controller + ReponseBody
 @RequestMapping("/books")
 public class BookController {
@@ -38,9 +41,29 @@ public class BookController {
 
   //@RequestMapping(method = RequestMethod.GET)
   @GetMapping
-  public String getAll() {
-    System.out.println("book getAll...");
-    return "{'module':'book getAll'}";
+  public List<Book> getAll() {
+    System.out.println("book getAll is running ...");
+    List<Book> bookList = new ArrayList<Book>();
+
+    Book book1 = new Book();
+    book1.setType("计算机");
+    book1.setName("SpringMVC入门教程");
+    book1.setDescription("小试牛刀");
+    bookList.add(book1);
+
+    Book book2 = new Book();
+    book2.setType("计算机");
+    book2.setName("SpringMVC实战教程");
+    book2.setDescription("一代宗师");
+    bookList.add(book2);
+
+    Book book3 = new Book();
+    book3.setType("计算机丛书");
+    book3.setName("SpringMVC实战教程进阶");
+    book3.setDescription("一代宗师呕心创作");
+    bookList.add(book3);
+
+    return bookList;
   }
 
 }
