585b6edebfa2beffcf6f95945e8741357b24bd3f
hefeng6500 <2443992009@qq.com>
2023-05-04 16:23:34 +0800

    feat: 简化拦截器配置使用 WebMvcConfigurer

--- ssm/src/main/java/com/example/config/SpringMvcConfig.java
+++ ssm/src/main/java/com/example/config/SpringMvcConfig.java
@@ -1,11 +1,32 @@
 package com.example.config;
 
+import com.example.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan({"com.example.controller", "com.example.config"})
 @EnableWebMvc
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+  @Autowired
+  private ProjectInterceptor projectInterceptor;
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+    registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+    registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins/");
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    // 配置拦截器
+    registry.addInterceptor(projectInterceptor).addPathPatterns("/books", "/books/*");
+  }
 }
