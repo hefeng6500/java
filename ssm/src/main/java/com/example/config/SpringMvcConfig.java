53907be305ad1f74785c10be2e354af08a47aac8
hefeng6500 <2443992009@qq.com>
2023-05-04 15:42:49 +0800

    feat: SpringMVC 增删改查页面案例

--- ssm/src/main/java/com/example/config/SpringMvcConfig.java
+++ ssm/src/main/java/com/example/config/SpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.example.controller")
+@ComponentScan({"com.example.controller", "com.example.config"})
 @EnableWebMvc
 public class SpringMvcConfig {
 }
--- ssm/src/main/java/com/example/service/impl/BookServiceImpl.java
+++ ssm/src/main/java/com/example/service/impl/BookServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.service.impl;
 
+import com.example.controller.Code;
 import com.example.dao.BookDao;
 import com.example.domain.Book;
+import com.example.exception.BusinessException;
 import com.example.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,21 +16,27 @@ public class BookServiceImpl implements BookService {
   private BookDao bookDao;
 
   public boolean save(Book book) {
-    bookDao.save(book);
-    return true;
+    return bookDao.save(book) > 0;
   }
 
   public boolean update(Book book) {
-    bookDao.update(book);
-    return true;
+    return bookDao.update(book) > 0;
   }
 
   public boolean delete(Integer id) {
-    bookDao.delete(id);
-    return true;
+    return bookDao.delete(id) > 0;
   }
 
   public Book getById(Integer id) {
+    if(id == 1){
+      throw new BusinessException(Code.BUSINESS_ERR,"请不要使用你的技术挑战我的耐性!");
+    }
+//        //将可能出现的异常进行包装，转换成自定义异常
+//        try{
+//            int i = 1/0;
+//        }catch (Exception e){
+//            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试!",e);
+//        }
     return bookDao.getById(id);
   }
 
