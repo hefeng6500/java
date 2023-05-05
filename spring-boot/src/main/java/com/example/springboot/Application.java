285c5f85290a0895d4e1325a0628ddddf3eab3a6
hefeng6500 <243992009@qq.com>
2023-05-05 17:32:09 +0800

    feat: spring boot yml 文件属性配置和读取

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -7,15 +7,16 @@ import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 
-@SpringBootApplication(exclude = ServletWebServerFactoryAutoConfiguration.class)
+// @SpringBootApplication(exclude = ServletWebServerFactoryAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
   // 配置Jetty服务器
-  @Bean
-  public JettyServletWebServerFactory jettyServletWebServerFactory() {
-    return new JettyServletWebServerFactory();
-  }
+  // @Bean
+  // public JettyServletWebServerFactory jettyServletWebServerFactory() {
+  //   return new JettyServletWebServerFactory();
+  // }
 }
