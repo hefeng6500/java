d7cb3120d11e52b1e331eb9a87e9a3f71a374be3
hefeng6500 <243992009@qq.com>
2023-05-05 17:41:04 +0800

    feat: spring boot yml 文件属性使用自定义对象自动注入

--- spring-boot/src/main/java/com/example/springboot/controller/BookController.java
+++ spring-boot/src/main/java/com/example/springboot/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.config.Enterprise;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -13,15 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
   @Autowired
-  private Environment env;
+  private Enterprise enterprise;
 
   @GetMapping("/{id}")
   public String getById(@PathVariable Integer id) {
     System.out.println("id ==> " + id);
 
-    System.out.println(env.getProperty("lesson"));
-    System.out.println(env.getProperty("enterprise.name"));
-    System.out.println(env.getProperty("enterprise.subject[0]"));
+    System.out.println(enterprise.getName());
+    System.out.println(enterprise.getAge());
+    System.out.println(enterprise.getSubject());
+    System.out.println(enterprise.getTel());
+    System.out.println(enterprise.getSubject()[0]);
 
     return "hello , spring boot!";
   }
