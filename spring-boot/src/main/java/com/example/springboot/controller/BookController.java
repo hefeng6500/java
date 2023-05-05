50de3e5b08aad11bd3dbd2e80b3d2d6f77761ff7
hefeng6500 <243992009@qq.com>
2023-05-05 17:33:39 +0800

    feat: spring boot yml 文件属性使用Environment 对象

--- spring-boot/src/main/java/com/example/springboot/controller/BookController.java
+++ spring-boot/src/main/java/com/example/springboot/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.example.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 public class BookController {
 
-  @Value("${lesson}")
-  private String lesson;
-  @Value("${server.port}")
-  private Integer port;
-  @Value("${enterprise.subject[0]}")
-  private String subject_00;
+  @Autowired
+  private Environment env;
 
   @GetMapping("/{id}")
-  public String getById(@PathVariable Integer id){
-    System.out.println("id ==> "+id);
+  public String getById(@PathVariable Integer id) {
+    System.out.println("id ==> " + id);
 
-    System.out.println(lesson);
-    System.out.println(port);
-    System.out.println(subject_00);
+    System.out.println(env.getProperty("lesson"));
+    System.out.println(env.getProperty("enterprise.name"));
+    System.out.println(env.getProperty("enterprise.subject[0]"));
 
     return "hello , spring boot!";
   }
