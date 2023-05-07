cbeed89d9f1b66d56879f761ef5d6cf2619fb638
hefeng6500 <2443992009@qq.com>
2023-05-07 10:52:02 +0800

    feat: MybatisPlus 实现增删改查

--- springboot-mybatisplus/sql.sql
+++ springboot-mybatisplus/sql.sql
@@ -1,15 +1,24 @@
+drop database if exists mybatisplus_db;
 create
-database if not exists mybatisplus_db;
+    database if not exists mybatisplus_db;
 
 use
-mybatisplus_db;
+    mybatisplus_db;
 
 create table user
 (
-    id       int,
-    name     varchar(32),
+    id       BIGINT(20)  NOT NULL COMMENT '主键ID',
+    name     VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
+    age      INT(11)     NULL DEFAULT NULL COMMENT '年龄',
     password varchar(32),
-    age      int,
-    tel      varchar(15)
+    tel      varchar(15),
+    PRIMARY KEY (id)
 );
+
+INSERT INTO user (id, name, age, password, tel)
+VALUES (1, 'Jone', 18, 'test1@baomidou.com', '18100009999'),
+       (2, 'Jack', 20, 'test2@baomidou.com', '18100009999'),
+       (3, 'Tom', 28, 'test3@baomidou.com', '18100009999'),
+       (4, 'Sandy', 21, 'test4@baomidou.com', '18100009999'),
+       (5, 'Billie', 24, 'test5@baomidou.com', '18100009999');
 
--- springboot-mybatisplus/src/test/java/com/example/springbootmybatisplus/SpringbootMybatisplusApplicationTests.java
+++ springboot-mybatisplus/src/test/java/com/example/springbootmybatisplus/SpringbootMybatisplusApplicationTests.java
@@ -19,6 +19,41 @@ class SpringbootMybatisplusApplicationTests {
   }
 
   @Test
+  void testSave(){
+    User user = new User();
+    user.setName("Yang");
+    user.setPassword("12345678");
+    user.setTel("'18800009999'");
+    user.setAge(22);
+
+    userDao.insert(user);
+  }
+
+  @Test
+  void testUpdate(){
+    User user = new User();
+    user.setId(1L);
+    user.setName("Jack Ma");
+    user.setPassword("999");
+    user.setTel("19922228888");
+
+    userDao.updateById(user);
+  }
+
+  @Test
+  void testDelete(){
+    Long id = 1655039397934583809L;
+
+    userDao.deleteById(id);
+  }
+
+  @Test
+  void getUserById(){
+    User user = userDao.selectById(1L);
+    System.out.println(user);
+  }
+
+  @Test
   void getAll() {
     List<User> users = userDao.selectList(null);
     System.out.println(users);
